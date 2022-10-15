7e5d2cd4ce1609447bac15f196fd22d47094b624
Nuspli <108233076+Nuspli@users.noreply.github.com>
2022-10-15 16:30:00 +0200

    Add files via upload

--- website_game/game.js
+++ website_game/game.js
@@ -1,23 +1,45 @@
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, snakeIntersection } from './snake.js'
+import { update as updateFood, draw as drawFood } from './food.js'
+import { outsideGrid } from './grid.js'
+
 let lastRenderTime = 0
-import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
+let gameOver = false
+const gameBoard = document.getElementById('game-board')
 
-function main(currenTime) {
-    const secondsSinceLastRender = (currenTime - lastRenderTime) / 1000
-    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
-    window.requestAnimationFrame(main)
-    lastRenderTime = currenTime
-    console.log('Render')
+function main(currentTime) {
+  if (gameOver) {
+    if (confirm('You lost. Press ok to restart.')) {
+      window.location = '/'
+    }
+    return
+  }
 
-    update()
-    draw()
+
+  window.requestAnimationFrame(main)
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+
+
+  lastRenderTime = currentTime
+
+  update()
+  draw()
 }
 
 window.requestAnimationFrame(main)
 
 function update() {
-    updateSnake
+  updateSnake()
+  updateFood()
+  checkDeath()
 }
 
 function draw() {
-    drawSnake
+  gameBoard.innerHTML = ''
+  drawSnake(gameBoard)
+  drawFood(gameBoard)
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection()
 }
