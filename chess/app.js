d98a8db743578ca990943081ae5c805702743f23
Nuspli <108233076+Nuspli@users.noreply.github.com>
2023-01-13 22:10:07 +0100

    frontend chess engine game structure
    all images by me, 
    pieces heavily inspired by lichess' horsey piece set

---
+++ chess/app.js
@@ -0,0 +1,25 @@
+const info = document.querySelector('.info__button');
+const infoText = document.querySelector('.info__container');
+
+info.addEventListener('click', function() {
+    info.classList.toggle('is-active');
+    infoText.classList.toggle('active');
+});
+
+const diff = document.querySelector('.diff__button');
+const diffText = document.querySelector('.diff__container');
+
+diff.addEventListener('click', function() {
+    diff.classList.toggle('is-active');
+    diffText.classList.toggle('active');
+});
+
+const play = document.querySelector('.play__button');
+
+play.addEventListener('click', function() {
+    var selected = document.getElementById('diff__select').value
+    if (selected == 1){localStorage.difficulty = 1}
+    else if (selected == 2){localStorage.difficulty = 2}
+    else {localStorage.difficulty = 3}
+    window.location.replace("chess.html");
+});
