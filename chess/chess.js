5e1764a2a97305871eedbf486b4c21d9276d191b
Nuspli <108233076+Nuspli@users.noreply.github.com>
2023-01-14 15:18:56 +0100

    cant take your own pieces anymore
    also found a way to restrict moves - I think

--- chess/chess.js
+++ chess/chess.js
@@ -2,47 +2,75 @@ let selectedPiece;
 
 // Function to handle the selection of a piece
 function dragStart(event) {
-  selectedPiece = event.target;
+    selectedPiece = event.target;
+    event.dataTransfer.setDragImage(event.target, event.target.offsetWidth / 2, event.target.offsetHeight / 2);
+    let x = event.clientX, y = event.clientY;
+    for (let i = 0; i < squares.length; i++) {
+        let rect = squares[i].getBoundingClientRect();
+        if (x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom) {
+        let dragSquare = i + 1;
+        console.log(`Piece was dragged from square: ${dragSquare}`);
+        break;
+        }
+    }
 }
 
 // Function to handle the placement of a piece
 function dragEnd(event) {
-  let targetSquare = event.target;
-  // Check if the target is a square
-  if (!targetSquare.classList.contains('square')) {
-    targetSquare = targetSquare.closest('.square');
-  }
-  if (targetSquare && selectedPiece) {
-    // Append the selected piece to the target square
-    targetSquare.appendChild(selectedPiece);
-    selectedPiece.style.left = 0;
-    selectedPiece.style.top = 0;
-    selectedPiece = null;
-  }
+    let targetSquare = event.target;
+    event.dataTransfer.setDragImage(event.target, event.target.offsetWidth / 2, event.target.offsetHeight / 2);
+    // Check if the target is a square
+    if (!targetSquare.classList.contains('square')) {
+        targetSquare = targetSquare.closest('.square');
+    }
+    if (targetSquare && selectedPiece) {
+        // Append the selected piece to the target square
+
+        targetSquare.appendChild(selectedPiece);
+        selectedPiece = null;
+    }
 }
 
 const squares = document.querySelectorAll('.square');
 const pieces = document.querySelectorAll('.piece');
+let color;
 
 // Add event listeners to each piece
 pieces.forEach(piece => {
-  piece.setAttribute("draggable", true);
-  piece.addEventListener('dragstart', dragStart);
-  piece.addEventListener('dragend', dragEnd);
+    piece.setAttribute("draggable", true);
+    piece.addEventListener('dragstart', dragStart);
+    piece.addEventListener('dragend', dragEnd);
 });
 
 // Add event listeners to each square
 squares.forEach(square => {
-  square.addEventListener('dragover', function(event) {
-    event.preventDefault();
-  });
-  square.addEventListener('drop', function(event) {
-    event.preventDefault();
-    if (square.hasChildNodes()) {
-      square.removeChild(square.firstChild);
-    }
-    square.appendChild(selectedPiece);
-  });
+    square.addEventListener('dragover', function(event) {
+        event.preventDefault();
+    });
+    square.addEventListener('drop', function(event) {
+        event.preventDefault();
+
+        if (square.lastElementChild) {color = square.lastElementChild.lastElementChild.lastElementChild.className}
+        else {color = 'no'}
+
+        if (color != 'white'){
+            if (square.hasChildNodes()) {
+                square.removeChild(square.firstChild);
+            }
+            square.appendChild(selectedPiece);
+
+            let x = event.clientX, y = event.clientY;
+            for (let i = 0; i < squares.length; i++) {
+                let rect = squares[i].getBoundingClientRect();
+                if (x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom) {
+                let dropSquare = i + 1;
+                console.log(`Piece was dropped on square: ${dropSquare}`);
+                break;
+                }
+            }
+        }
+        
+    });
 });
 
 const retry = document.querySelector('.retry_button');
